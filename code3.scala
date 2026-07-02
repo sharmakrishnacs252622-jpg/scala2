c711d22d66b5559c9e62f4f9047a20c03bac1c6b
sharmakrishnacs252622-jpg <sharmakrishnacs252622@gmail.com>
2026-07-02 12:34:16 +0530

    Create code3.scala

---
+++ code3.scala
@@ -0,0 +1,26 @@
+import scala.util.Random
+
+object VarianceStdDev {
+  def mean(list: List[Double]): Double = {
+    list.sum / list.length
+  }
+
+  def variance(list: List[Double]): Double = {
+    val m = mean(list)
+    list.map(x => math.pow(x - m, 2)).sum / list.length
+  }
+
+  def stdDev(list: List[Double]): Double = {
+    math.sqrt(variance(list))
+  }
+
+  def main(args: Array[String]): Unit = {
+    val random = new Random()
+    val numbers = List.fill(10)(random.nextInt(100).toDouble)
+
+    println("Random Numbers: " + numbers)
+    println("Mean: " + mean(numbers))
+    println("Variance: " + variance(numbers))
+    println("Standard Deviation: " + stdDev(numbers))
+  }
+}
